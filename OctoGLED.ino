cb4df2646dff40a44cd08748047d5c8e9e349605
Joakim Hembrink <jh@auranest.com>
2015-12-03 11:29:48 +0100

    Added colour crrection

--- OctoGLED.ino
+++ OctoGLED.ino
@@ -6,18 +6,21 @@
 #define NUM_LEDS_PER_STRIP 256
 #define NUM_STRIPS 8
 #define CMD_NEW_DATA 1
+#define MAX_BRIGHTNESS 255
 
 CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 
-void setup() {
-	FastLED.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP);
-	FastLED.setBrightness(100);
+void setup()
+{
+	FastLED.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP).setCorrection( TypicalPixelString );
+	FastLED.setBrightness( MAX_BRIGHTNESS );
 
-	Serial.begin(9600);
+	Serial.begin( 9600 );
 }
 
-void loop() {
-	while (!Serial.available() || Serial.read() != CMD_NEW_DATA);
-	Serial.readBytes((char*)leds, NUM_LEDS_PER_STRIP * NUM_STRIPS * 3);
+void loop()
+{
+	while ( !Serial.available() || Serial.read() != CMD_NEW_DATA );
+	Serial.readBytes( (char*)leds, NUM_LEDS_PER_STRIP * NUM_STRIPS * 3 );
 	FastLED.show();
 }
