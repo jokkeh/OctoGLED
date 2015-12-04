4c01f2b151db6ee239c571760014f4b81e8d19a9
Joakim Hembrink <jh@auranest.com>
2015-12-04 15:17:21 +0100

    Removed the darn tabs, #teamSpaces. Also added som comments

--- OctoGLED.ino
+++ OctoGLED.ino
@@ -3,24 +3,31 @@
 // Must be FastLED 3.1+
 #include<FastLED.h>
 
+// Change these according to your setup
 #define NUM_LEDS_PER_STRIP 256
 #define NUM_STRIPS 8
-#define CMD_NEW_DATA 1
+
+// This can be adjusted in the control software, but one could also set
+// a limit here. Don't burn your power supplies!
 #define MAX_BRIGHTNESS 255
+
+// Don't change this
+#define CMD_NEW_DATA 1
 
 CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 
 void setup()
 {
-	FastLED.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP).setCorrection( TypicalPixelString );
-	FastLED.setBrightness( MAX_BRIGHTNESS );
+    FastLED.addLeds<WS2811_PORTD, NUM_STRIPS>(leds, NUM_LEDS_PER_STRIP).setCorrection( TypicalPixelString );
+    FastLED.setBrightness( MAX_BRIGHTNESS );
 
-	Serial.begin( 9600 );
+    // Seems baud rate doesn't really matter
+    Serial.begin( 9600 );
 }
 
 void loop()
 {
-	while ( !Serial.available() || Serial.read() != CMD_NEW_DATA );
-	Serial.readBytes( (char*)leds, NUM_LEDS_PER_STRIP * NUM_STRIPS * 3 );
-	FastLED.show();
+    while ( !Serial.available() || Serial.read() != CMD_NEW_DATA );
+    Serial.readBytes( (char*)leds, NUM_LEDS_PER_STRIP * NUM_STRIPS * 3 );
+    FastLED.show();
 }
